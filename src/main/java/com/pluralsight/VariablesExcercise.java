efa9a6eb6b875e582f28ab5f339cdfa79428582a
lewiskangethe <lewiskkangethe@gmail.com>
2025-09-24 15:54:09 -0400

    initial commit

---
+++ src/main/java/com/pluralsight/VariablesExcercise.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class VariablesExcercise {
+    int vin = 5265489;
+    String vehicle_make = "VW Tiguan";
+    String vehicle_color = "black";
+    boolean towing_package = true;
+    int odometer = 67500;
+    double price = 18999.99;
+    String quality_rating = "B";
+    String phone_number = "470-559-6597";
+    String social_security = "568-87-9664";
+    String zip_code = "35569-5698";
+
+
+
+}
